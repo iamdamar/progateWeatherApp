c7a74470cd1f43af185aff2517960dd2e023310d
damarwu <hello.damarwu@gmail.com>
2024-06-25 20:27:08 +0700

    succesfully developed loading, success and error state in the UI

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React, {useState} from "react"
 import axios from "axios"
 import {BASE_URL, API_KEY} from './src/constant'
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, ActivityIndicator, Text} from 'react-native'
 import WeatherSearch from './src/components/weatherSearch'
 import WeatherInfo from './src/components/weatherInfo'
 
 const App = () => {
   const [weatherData, setWeatherData] = useState()
+  const [status, setStatus] = useState('')
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size='large' />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return
+    }
+  }
+
   const searchWeather = (location) => {
+    setStatus('loading')
     axios
       .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
       .then((response) => {
@@ -17,16 +36,19 @@ const App = () => {
         data.main.temp -= 273.15
         data.main.temp = data.main.temp.toFixed(2)
         setWeatherData(data)
+        setStatus('success')
       })
       .catch((error) => {
-        console.log(error)
+        setStatus('error')
       })
   }
 
   return (
     <View style={styles.container}>
       <WeatherSearch searchWeather={searchWeather} />
-      {weatherData && <WeatherInfo weatherData={weatherData} />}
+      <View style={styles.marginTop20}>
+        {renderComponent()}
+      </View>
     </View>
   )
 }
