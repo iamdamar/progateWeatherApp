fee687c3c2ceec4be4f03123ee576517d1473790
damarwu <hello.damarwu@gmail.com>
2024-06-25 14:39:52 +0700

    succesfully developed the search feature

--- src/components/weatherSearch.js
+++ src/components/weatherSearch.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {View, Button, StyleSheet} from 'react-native'
 import CustomTextInput from './customTextInput'
 
-const WeatherSearch = () => {
+const WeatherSearch = ({searchWeather}) => {
+    const [location, setLocation] = useState('')
+
     return (
         <View>
             <CustomTextInput
                 placeholder="Search the weather of your city"
                 numberOfLines={1}
+                text={location}
+                onChange={setLocation}
             />
             <View style={styles.buttonWrapper}>
                 <Button
                     title="Search"
-                    onPress={() => {}}
+                    onPress={() => searchWeather(location)}
                 />
             </View>
         </View>
